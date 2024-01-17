0f14df0a000a5d904088a92988d0432d76e19557
ismnarmin13 <narminvi@code.edu.az>
2024-01-18 00:26:12 +0400

    commit

--- switch.js
+++ switch.js
@@ -1,4 +1,22 @@
-function toggleSwitch() {
-    const toggleSwitch = document.querySelector('.toggle-switch');
-    toggleSwitch.classList.toggle('active');
-  }
+const circles = document.querySelectorAll(".circle");
+
+circles.forEach((circle) => {
+  circle.addEventListener("click", function () {
+    document.querySelectorAll(".mini-circle").forEach((miniCircle) => {
+      miniCircle.style.display = "none";
+    });
+
+    const miniCircle = circle.querySelector(".mini-circle");
+    miniCircle.style.display = "block";
+
+    circles.forEach((c) => {
+      c.style.borderColor = "black";
+    });
+
+    circle.style.borderColor = "blue";
+  });
+});
+
+function toggleSwitch(element) {
+  element.classList.toggle('active');
+}
